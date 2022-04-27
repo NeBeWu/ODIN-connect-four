b81c34e517d11095abea22fdac85640db2151765
Pedro <nebewu@gmail.com>
2022-04-27 10:27:28 -0300

    Refactor Interface module and add tests

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,17 +1,61 @@
 # frozen_string_literal: true
 
 module Interface
-  def show
-    6.times do |row|
-      print "-----------------------------\n"
+  def fetch_input(message, error_message, validation)
+    puts message
+    input = gets.chomp
 
-      @columns.each do |column|
-        print "| #{column[row]} "
-      end
-
-      print "|\n"
+    until send(validation, input)
+      puts error_message
+      input = gets.chomp
     end
 
-    print "-----------------------------\n"
+    input
+  end
+
+  module Grid
+    def show
+      6.times do |row|
+        print "-----------------------------\n"
+
+        @columns.each do |column|
+          print "| #{column[row]} "
+        end
+
+        print "|\n"
+      end
+
+      print "-----------------------------\n"
+    end
+  end
+
+  module Player
+    def validate_name(name)
+      name.match?(/^\w{1,10}$/)
+    end
+
+    def validate_token(token)
+      token.match?(/^[a-zA-Z]$/)
+    end
+
+    def fetch_name
+      input = fetch_input(
+        'Please, enter your name player 1.',
+        'Wrong input! Please enter 1 to 10 word characters.',
+        :validate_name
+      )
+
+      insert_name(input)
+    end
+
+    def fetch_token
+      input = fetch_input(
+        'Please, enter your token player 1.',
+        'Wrong input! Please enter a non-numeric word character.',
+        :validate_token
+      )
+
+      insert_token(input)
+    end
   end
 end
